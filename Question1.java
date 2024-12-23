51d0fb8d1bedeaf8b11781af4823dbed4034044c
ArkaMandal2003 <arkamandalofficial@gmail.com>
2024-12-23 20:12:54 +0530

    did a question

---
+++ Question1.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+// q1. implement s= ut + 1/2 at^2
+public class Question1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter the initial speed:");
+        double speed = scanner.nextDouble();
+
+        System.out.println("enter the time:");
+        double time = scanner.nextDouble();
+
+        System.out.println("enter the accelaration:");
+        double acc = scanner.nextDouble();
+
+        double distance = (speed * time) + (0.5 * acc * time * time);
+        System.out.println("The distance covered is: "+ distance);
+
+    }
+}
